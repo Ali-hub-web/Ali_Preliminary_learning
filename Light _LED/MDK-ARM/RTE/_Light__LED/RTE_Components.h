296fdf2e99a3e23675e7028d8cf0a131ebb584b8
ali-han <alisoya1@outlook.com>
2025-06-04 20:54:21 +0800

    This is the first project created by han.

---
+++ Light _LED/MDK-ARM/RTE/_Light__LED/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Light _LED' 
+ * Target:  'Light _LED' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
